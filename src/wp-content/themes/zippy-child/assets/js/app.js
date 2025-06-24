342724cde825acdee612148dcf65a4030186a543
Nhanlt <trongnhanle5004@gmail.com>
2025-06-24 11:29:30 +0700

    Update feature on slider

--- src/wp-content/themes/zippy-child/assets/js/app.js
+++ src/wp-content/themes/zippy-child/assets/js/app.js
@@ -4,12 +4,13 @@ import "../lib/slick/slick.min.js";
 ("use strict");
 $ = jQuery;
 $(document).ready(function () {
-  console.log("ready");
-  $('.featured-on-slider .col-inner').slick({
+  const $slider = $(".featured-on-slider .col-inner");
+
+  // Init slick
+  $slider.slick({
     speed: 3000,
-    autoplay: true,
-    autoplaySpeed: 0,
-    cssEase: 'linear',
+    autoplay: false,
+    cssEase: "linear",
     slidesToShow: 1,
     slidesToScroll: 1,
     variableWidth: true,
@@ -17,5 +18,49 @@ $(document).ready(function () {
     arrows: false,
     pauseOnHover: false,
     pauseOnFocus: false,
+  });
+
+  const $track = $slider.find(".slick-track");
+
+  let velocity = 1.2;
+  let offset = 0;
+  let isHovering = false;
+
+  function animate() {
+    offset += velocity;
+    const trackWidth = $track.width() / 2;
+
+    if (Math.abs(offset) >= trackWidth) {
+      offset = 0;
+    }
+
+    $track.css("transform", `translate3d(${-offset}px, 0, 0)`);
+    requestAnimationFrame(animate);
+  }
+
+  $track.html($track.html() + $track.html());
+
+  animate();
+  $slider.on("mousemove", function (e) {
+    const sliderOffset = $slider.offset().left;
+    const sliderWidth = $slider.outerWidth();
+    const center = sliderOffset + sliderWidth / 2;
+    const mouseX = e.pageX;
+    const dist = mouseX - center;
+
+    const maxSpeed = 1;
+
+    velocity = (dist / (sliderWidth / 2)) * maxSpeed;
+
+    if (Math.abs(velocity) < 0.2) {
+      velocity = 0;
+    }
+
+    isHovering = true;
+  });
+
+  $slider.on("mouseleave", function () {
+    velocity = 1.2;
+    isHovering = false;
   });
 });

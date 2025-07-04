18922f08855f88019193fdb935be13786bff5dc9
toanpham2111 <toan444666@gmail.com>
2025-07-04 13:37:14 +0700

    quick update

--- src/wp-content/themes/index.php
+++ src/wp-content/themes/index.php
@@ -1,2 +1,2 @@
-<h3 class="count-up">70.8%</h3>
-<p>of business cost for a retailer goes to purchasing of goods and material</p>
+<?php
+// Silence is golden.
--- src/wp-content/themes/zippy-child/includes/shin_admin.php
+++ src/wp-content/themes/zippy-child/includes/shin_admin.php
@@ -57,5 +57,3 @@ add_action('init', function () {
         remove_action('admin_bar_menu', 'wp_admin_bar_comments_menu', 60);
     }
 });
-
-?>

c40923f7c5b3f6d76958b4c86e300ecbb7d0b073
toanpham2111 <toan444666@gmail.com>
2025-07-08 17:40:00 +0700

    update

---
+++ src/wp-content/themes/zippy-child/includes/zippy_footer.php
@@ -0,0 +1,13 @@
+<?php
+function footer_shortcode()
+{
+
+    $current_url = $_SERVER['REQUEST_URI'];
+
+    if (trim($current_url, '/') === 'my') {
+        return do_shortcode('[block id="footer-malaysia"]');
+    } else {
+        return do_shortcode('[block id="footer"]');
+    }
+}
+add_shortcode('custom_footer', 'footer_shortcode');

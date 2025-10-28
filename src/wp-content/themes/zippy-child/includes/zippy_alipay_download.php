b8dfc67c29f341ae8794cf70bf1d31ca8a71ac7a
shin <nhathanh024@gmail.com>
2025-10-28 21:44:26 +0700

    add alipay page

--- src/wp-content/themes/zippy-child/includes/zippy_alipay_download.php
+++ src/wp-content/themes/zippy-child/includes/zippy_alipay_download.php
@@ -2,21 +2,29 @@
 
 // === Custom URL: /page/landing/download.html ===
 add_action('init', function () {
-  add_rewrite_rule(
-    '^page/landing/download\.html$',
-    'index.php?alipay-download=1',
-    'top'
-  );
+    add_rewrite_rule(
+        'page/landing/download\.html$',
+        'index.php?alipay-download=1',
+        'top'
+    );
 });
 
 add_filter('query_vars', function ($vars) {
-  $vars[] = 'alipay-download';
-  return $vars;
+    $vars[] = 'alipay-download';
+    return $vars;
 });
 
 add_action('template_redirect', function () {
-  if (get_query_var('alipay-download')) {
-    include THEME_DIR  . '-child' .  '/alipay-dowload.php';
-    exit;
-  }
+    if (get_query_var('alipay-download')) {
+        include THEME_DIR  . '-child' .  '/alipay-dowload.php';
+        exit;
+    }
 });
+
+add_filter('user_trailingslashit', function ($url, $type) {
+    // Remove trailing slash only for our dowload.html
+    if (strpos($url, 'page/landing/download.html') !== false) {
+        return untrailingslashit($url);
+    }
+    return $url;
+}, 10, 2);

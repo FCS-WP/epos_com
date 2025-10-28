92775eedebc7c08a05f093b73bd833909f5b3ddd
shin <nhathanh024@gmail.com>
2025-10-28 21:29:56 +0700

    add alipay

---
+++ src/wp-content/themes/zippy-child/alipay-dowload.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Template for /page/landing/download.html
+ */
+?>
+
+<iframe
+  src="https://pre.ac.alipay.com/page/landing/download.html"
+  width="100%"
+  height="900"
+  style="border:0;"
+  loading="lazy"
+  title="Alipay Download"></iframe>

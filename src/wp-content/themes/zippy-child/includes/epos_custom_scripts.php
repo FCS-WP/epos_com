4553ab4ed02f5ec1aa9c85d17432b87b72bee979
nhanlt-FLS <nhan.letrong@floatingcube.com>
2026-01-21 18:29:47 +0700

    Update GTM scripts for epos.com and epos.com/my

--- src/wp-content/themes/zippy-child/includes/epos_custom_scripts.php
+++ src/wp-content/themes/zippy-child/includes/epos_custom_scripts.php
@@ -3,22 +3,32 @@ add_action('wp_head', function () {
     $uri = $_SERVER['REQUEST_URI'] ?? '';
 
     if (strpos($uri, '/my/') !== 0) {
-        return;
+        ?>
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-TBKNQPPH');</script>
+        <!-- End Google Tag Manager -->
+        <?php
+        
+    } else {
+        ?>
+        <!-- Google Tag Manager -->
+        <script>
+        (function(w,d,s,l,i){w[l]=w[l]||[];
+        w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+        var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+        j.async=true;
+        j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+        f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-P4PQK8KM');
+        </script>
+        <!-- End Google Tag Manager -->
+        <?php
     }
-    ?>
-    <!-- Google Tag Manager -->
-    <script>
-    (function(w,d,s,l,i){w[l]=w[l]||[];
-    w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-    var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-    j.async=true;
-    j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-    f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-P4PQK8KM');
-    </script>
-    <!-- End Google Tag Manager -->
-    <?php
 });
 
 
@@ -26,14 +36,20 @@ add_action('wp_body_open', function () {
     $uri = $_SERVER['REQUEST_URI'] ?? '';
 
     if (strpos($uri, '/my/') !== 0) {
-        return;
+        ?>
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TBKNQPPH"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
+        <?php
+    } else { 
+        ?>
+        <!-- Google Tag Manager (noscript) -->
+        <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P4PQK8KM"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe>
+        </noscript>
+        <!-- End Google Tag Manager (noscript) -->
+        <?php
     }
-    ?>
-    <!-- Google Tag Manager (noscript) -->
-    <noscript>
-      <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P4PQK8KM"
-      height="0" width="0" style="display:none;visibility:hidden"></iframe>
-    </noscript>
-    <!-- End Google Tag Manager (noscript) -->
-    <?php
 });

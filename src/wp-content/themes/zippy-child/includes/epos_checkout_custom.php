8363119e1042f8353271a3f6cdf5d1f82d54cc2c
nhanlt-FLS <nhan.letrong@floatingcube.com>
2026-01-21 18:53:48 +0700

    Create extra MCC/UNE field for checkout order info

--- src/wp-content/themes/zippy-child/includes/epos_checkout_custom.php
+++ src/wp-content/themes/zippy-child/includes/epos_checkout_custom.php
@@ -1,4 +1,5 @@
 <?php
+// Action
 // Enqueue scripts for billing phone country codes
 add_action('wp_enqueue_scripts', function () {
     if (!is_checkout()) return;
@@ -19,7 +20,47 @@ add_action('wp_enqueue_scripts', function () {
     );
 });
 
+// Custom order MCC/UEN field for checkout
+add_action('woocommerce_before_order_notes', function ($checkout) {
+    woocommerce_form_field('order_tag', [
+        'type'        => 'text',
+        'class'       => ['form-row-wide'],
+        'label'       => __('E.g.MCC, UEN'),
+        'placeholder' => __('MCC/UEN'),
+        'required'    => false,
+    ], $checkout->get_value('order_tag'));
+});
+add_action('woocommerce_checkout_create_order', function ($order) {
+    if (!empty($_POST['order_tag'])) {
+        $order->update_meta_data(
+            'Tag',
+            sanitize_text_field($_POST['order_tag'])
+        );
+    }
+});
+// Show in order dashboard
+add_action('woocommerce_admin_order_data_after_billing_address', function ($order) {
+    $tag = $order->get_meta('Tag');
+    if ($tag) {
+        echo '<p><strong>Tag:</strong> ' . esc_html($tag) . '</p>';
+    }
+});
+// Show in mail
+add_filter('woocommerce_email_order_meta_fields', function ($fields, $sent_to_admin, $order) {
+    $tag = $order->get_meta('Tag');
+    if ($tag) {
+        $fields['order_tag'] = [
+            'label' => __('Tag'),
+            'value' => $tag,
+        ];
+    }
+    return $fields;
+}, 10, 3);
 
+
+
+
+// Filter
 // Company name input is required
 add_filter('woocommerce_checkout_fields', function($fields) {
     $fields['billing']['billing_company']['required'] = true;

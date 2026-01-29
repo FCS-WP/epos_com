a18c937b478edd649491b03c99487b1345b4bce3
shin <nhathanh024@gmail.com>
2026-01-29 09:33:18 +0700

    Update Payment Pipline and Dealstage

--- src/wp-content/themes/zippy-child/includes/hubspot_intergration/services.php
+++ src/wp-content/themes/zippy-child/includes/hubspot_intergration/services.php
@@ -45,7 +45,8 @@ function create_hubspot_deal($access_token, $deal_data, $contact_id)
       'amount'     => $deal_data['properties']['amount'],
       'closedate'  => $deal_data['properties']['closedate'],
       'dealstage'  => $deal_data['properties']['dealstage'],
-      'pipeline'   => 'default',
+      'pipeline'   => $deal_data['properties']['pipeline'],
+      'pi_number' => $deal_data['properties']['pi_number'],
     ],
     'associations' => [
       [

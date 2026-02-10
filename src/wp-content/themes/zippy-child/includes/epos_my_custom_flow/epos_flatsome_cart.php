1666b714b6c87419691adc39f112bc8326486a6a
shin <nhathanh024@gmail.com>
2026-02-10 18:00:23 +0700

    Deploy new version

--- src/wp-content/themes/zippy-child/includes/epos_my_custom_flow/epos_flatsome_cart.php
+++ src/wp-content/themes/zippy-child/includes/epos_my_custom_flow/epos_flatsome_cart.php
@@ -133,7 +133,7 @@ function custom_cart_item_name($product_name, $cart_item, $cart_item_key)
     $product = $cart_item['data'];
     if ($product && is_a($product, 'WC_Product')) {
         if ($product->get_id() == 2174) {
-            $product_name =  $product_name . '<div class="cart-pre-order">' . 'Pre-order – Delivery starts from February</div>';
+            $product_name =  $product_name . '<div class="cart-pre-order">' . 'Pre-order - Delivery starts from late February</div>';
         }
     }
     return $product_name;

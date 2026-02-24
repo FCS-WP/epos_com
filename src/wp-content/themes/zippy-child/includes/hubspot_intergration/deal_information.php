0f8c952534be5b73629e8553e5521fad8abd2fc4
shin <nhathanh024@gmail.com>
2026-02-24 09:04:01 +0700

    Add new field Referral code

--- src/wp-content/themes/zippy-child/includes/hubspot_intergration/deal_information.php
+++ src/wp-content/themes/zippy-child/includes/hubspot_intergration/deal_information.php
@@ -22,6 +22,7 @@ function sync_wc_paid_order_to_hubspot_deal($order_id, $order)
       'pipeline'     => '781854069',
       'dealstage'    => '1142728054',
       'pi_number'    => $order->get_order_number(),
+      'referral_code'       => $order->get_meta('referral_code'),
       'merchat_name__payment_team_' => $order->get_billing_company(),
       'closedate'    => $order->get_date_completed() ? $order->get_date_completed()->format('Y-m-d\TH:i:s\Z') : current_time('Y-m-d\TH:i:s\Z'),
     ],

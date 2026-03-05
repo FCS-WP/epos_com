87046684c25c7442d4a0caade2b84912aa9d0b4e
Nhanlt-FCS <nhan.letrong@floatingcube.com>
2026-03-05 14:35:59 +0700

    Implement Language Translate feature

--- src/wp-content/themes/zippy-child/includes/epos_my_custom_flow/epos_checkout_custom.php
+++ src/wp-content/themes/zippy-child/includes/epos_my_custom_flow/epos_checkout_custom.php
@@ -34,7 +34,7 @@ add_action('woocommerce_after_checkout_validation', function ($data, $errors) {
     if (!email_domain_is_exist($data['billing_email'])) {
         $errors->add(
             'billing_email',
-            __('<strong>Email domain</strong> does not exist. Please use a valid email address.', 'woocommerce')
+            __('<strong>' . Lang::translate('Email address') . '</strong>' . Lang::translate(' does not exist. Please use a valid email address.'), 'woocommerce')
         );
     }
 }, 10, 2);
@@ -60,12 +60,12 @@ add_action('woocommerce_checkout_create_order', function($order, $data) {
 // Company name input is required
 add_filter('woocommerce_checkout_fields', function($fields) {
     $fields['billing']['billing_company']['required'] = true;
-    $fields['billing']['billing_company']['label'] = 'Company name';
+    $fields['billing']['billing_company']['label'] = Lang::translate('Company name');
     return $fields;
 });
 add_filter('woocommerce_checkout_required_field_notice', function ($message, $field_label) {
     if ($field_label === 'Billing Email address') {
-        return '<strong>Email address</strong> is required.';
+        return '<strong>' . Lang::translate('Email address') . '</strong>' . Lang::translate(' is required.');
     }
     return $message;
 }, 10, 2);
@@ -74,7 +74,7 @@ add_filter('woocommerce_checkout_required_field_notice', function ($message, $fi
 // Email address input is required
 add_filter('woocommerce_checkout_required_field_notice', function ($message, $field_label) {
     if ($field_label === 'Billing Company name') {
-        return '<strong>Company name</strong> is required.';
+        return '<strong>' . Lang::translate('Company name') . '</strong>' . Lang::translate(' is required.');
     }
     return $message;
 }, 10, 2);
@@ -89,7 +89,7 @@ add_filter('woocommerce_default_address_fields', function ($fields) {
 add_filter('woocommerce_checkout_fields', function ($fields) {
     $fields['billing']['billing_full_name'] = [
         'type'        => 'text',
-        'label'       => 'Full name',
+        'label'       => Lang::translate('Full name'),
         'required'    => true,
         'priority'    => 10,
         'class'       => ['form-row-wide'],
@@ -99,7 +99,7 @@ add_filter('woocommerce_checkout_fields', function ($fields) {
 });
 
 add_filter('woocommerce_add_error', function ($message) {
-    return str_replace('Billing Full name', 'Full name', $message);
+    return str_replace('Billing Full name', Lang::translate('Full name'), $message);
 });
 
 

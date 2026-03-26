188602604f1463181e365fbffd1e1a936fa3e1b6
vince-8 <vince.vinhsam@ant-intl.com>
2026-03-26 18:34:35 +0700

    Fix tax rate label

--- src/wp-content/themes/zippy-child/woocommerce/cart/cart-totals.php
+++ src/wp-content/themes/zippy-child/woocommerce/cart/cart-totals.php
@@ -65,12 +65,6 @@ defined( 'ABSPATH' ) || exit;
 		<?php
 		if ( wc_tax_enabled() && ! WC()->cart->display_prices_including_tax() ) {
 			$taxable_address = WC()->customer->get_taxable_address();
-			$estimated_text  = '';
-
-			if ( WC()->customer->is_customer_outside_base() && ! WC()->customer->has_calculated_shipping() ) {
-				/* translators: %s location. */
-				$estimated_text = sprintf( ' <small>' . esc_html__( '(estimated for %s)', 'woocommerce' ) . '</small>', WC()->countries->estimated_for_prefix( $taxable_address[0] ) . WC()->countries->countries[ $taxable_address[0] ] );
-			}
 
 			if ( 'itemized' === get_option( 'woocommerce_tax_total_display' ) ) {
 				foreach ( WC()->cart->get_tax_totals() as $code => $tax ) { // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
@@ -78,7 +72,7 @@ defined( 'ABSPATH' ) || exit;
 					$percent = $rate['tax_rate'] ?? '';
 					?>
 					<tr class="tax-rate tax-rate-<?php echo esc_attr( sanitize_title( $code ) ); ?>">
-						<th><?php echo esc_html( $tax->label ) . $estimated_text . ' (' . (int)$percent . '%)'; ?></th>
+						<th><?php echo esc_html( $tax->label ) . ' (' . (int)$percent . '%)'; ?></th>
 						<td data-title="<?php echo esc_attr( $tax->label ); ?>"><?php echo wp_kses_post( $tax->formatted_amount ); ?></td>
 					</tr>
 					<?php

9d7c72592393ee8be8ff3b2721df03c640b6a8b3
Nhanlt-FCS <nhan.letrong@floatingcube.com>
2026-03-30 11:36:09 +0700

    Update back to checkout button adn change context payment secure to 2c2p

--- src/wp-content/themes/zippy-child/woocommerce/cart/cart.php
+++ src/wp-content/themes/zippy-child/woocommerce/cart/cart.php
@@ -184,7 +184,7 @@ $sidebar_classes = implode( ' ', $sidebar_classes );
 
 			<div class="cart-secure-badge">
 				<img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/icons/safe.svg" alt="Secure checkout">
-				<?php esc_html_e( 'Secure checkout powered by Antom', 'woocommerce' ); ?>
+				<?php esc_html_e( 'Secure checkout powered by 2c2p', 'woocommerce' ); ?>
 			</div>
 
 		</div>
